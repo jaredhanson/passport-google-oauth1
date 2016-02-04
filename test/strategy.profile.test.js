92cf2772ed68b80eaf89875459e4b0d29b1cb723
Jared Hanson <jaredhanson@gmail.com>
2016-02-03 18:14:13 -0800

    Add profile test case.

--- test/strategy.profile.test.js
+++ test/strategy.profile.test.js
@@ -4,6 +4,49 @@ var GoogleStrategy = require('../lib/strategy')
 
 describe('Strategy#userProfile', function() {
   
+  describe('fetched from default endpoint', function() {
+    var strategy = new GoogleStrategy({
+      consumerKey: 'www.example.com',
+      consumerSecret: 'secret'
+    }, function verify(){});
+    
+    strategy._oauth.get = function(url, token, tokenSecret, callback) {
+      if (url != 'https://www.google.com/m8/feeds/contacts/default/full?max-results=1&alt=json') { return callback(new Error('incorrect url argument')); }
+      if (token != 'token') { return callback(new Error('incorrect token argument')); }
+      if (tokenSecret != 'token-secret') { return callback(new Error('incorrect tokenSecret argument')); }
+    
+      var body = '{"version":"1.0","encoding":"UTF-8","feed":{"xmlns":"http://www.w3.org/2005/Atom","xmlns$openSearch":"http://a9.com/-/spec/opensearchrss/1.0/","xmlns$gContact":"http://schemas.google.com/contact/2008","xmlns$batch":"http://schemas.google.com/gdata/batch","xmlns$gd":"http://schemas.google.com/g/2005","id":{"$t":"example@gmail.com"},"updated":{"$t":"2012-01-10T19:21:31.537Z"},"category":[{"scheme":"http://schemas.google.com/g/2005#kind","term":"http://schemas.google.com/contact/2008#contact"}],"title":{"type":"text","$t":"Jared Hanson\'s Contacts"},"link":[{"rel":"alternate","type":"text/html","href":"http://www.google.com/"},{"rel":"http://schemas.google.com/g/2005#feed","type":"application/atom+xml","href":"https://www.google.com/m8/feeds/contacts/example%40gmail.com/full"},{"rel":"http://schemas.google.com/g/2005#post","type":"application/atom+xml","href":"https://www.google.com/m8/feeds/contacts/example%40gmail.com/full"},{"rel":"http://schemas.google.com/g/2005#batch","type":"application/atom+xml","href":"https://www.google.com/m8/feeds/contacts/example%40gmail.com/full/batch"},{"rel":"self","type":"application/atom+xml","href":"https://www.google.com/m8/feeds/contacts/example%40gmail.com/full?alt\\u003djson\\u0026max-results\\u003d1"},{"rel":"next","type":"application/atom+xml","href":"https://www.google.com/m8/feeds/contacts/example%40gmail.com/full?alt\\u003djson\\u0026start-index\\u003d2\\u0026max-results\\u003d1"}],"author":[{"name":{"$t":"Jared Hanson"},"email":{"$t":"example@gmail.com"}}],"generator":{"version":"1.0","uri":"http://www.google.com/m8/feeds","$t":"Contacts"},"openSearch$totalResults":{"$t":"3"},"openSearch$startIndex":{"$t":"1"},"openSearch$itemsPerPage":{"$t":"1"},"entry":[{"id":{"$t":"http://www.google.com/m8/feeds/contacts/example%40gmail.com/base/0"},"updated":{"$t":"2009-09-30T01:58:35.769Z"},"category":[{"scheme":"http://schemas.google.com/g/2005#kind","term":"http://schemas.google.com/contact/2008#contact"}],"title":{"type":"text","$t":""},"link":[{"rel":"http://schemas.google.com/contacts/2008/rel#edit-photo","type":"image/*","href":"https://www.google.com/m8/feeds/photos/media/example%40gmail.com/0/0XXxxXXxXxXxx0XX0xXxXx"},{"rel":"http://schemas.google.com/contacts/2008/rel#photo","type":"image/*","href":"https://www.google.com/m8/feeds/photos/media/example%40gmail.com/0"},{"rel":"self","type":"application/atom+xml","href":"https://www.google.com/m8/feeds/contacts/example%40gmail.com/full/0"},{"rel":"edit","type":"application/atom+xml","href":"https://www.google.com/m8/feeds/contacts/example%40gmail.com/full/0/1111111111111000"}],"gd$email":[{"rel":"http://schemas.google.com/g/2005#other","address":"example+other@gmail.com","primary":"true"}]}]}}';
+      var response = {headers:{'x-access-level': 'read'}};
+      callback(null, body, response);
+    }
+    
+    
+    var profile;
+  
+    before(function(done) {
+      strategy.userProfile('token', 'token-secret', { user_id: '6253282' }, function(err, p) {
+        if (err) { return done(err); }
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should parse profile', function() {
+      expect(profile.provider).to.equal('google');
+      expect(profile.id).to.equal('example@gmail.com');
+      expect(profile.displayName).to.equal('Jared Hanson');
+      expect(profile.emails[0].value).to.equal('example@gmail.com');
+    });
+  
+    it('should set raw property', function() {
+      expect(profile._raw).to.be.a('string');
+    });
+  
+    it('should set json property', function() {
+      expect(profile._json).to.be.an('object');
+    });
+  }); // fetched from default endpoint
+  
   describe('error caused by malformed response', function() {
     var strategy = new GoogleStrategy({
       consumerKey: 'www.example.com',
