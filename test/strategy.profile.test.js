24cc7ef72b8587c1d900461f236ab4af7ac76b2b
Jared Hanson <jaredhanson@gmail.com>
2016-02-03 18:00:50 -0800

    Add test cases for user profile error.

---
+++ test/strategy.profile.test.js
@@ -0,0 +1,72 @@
+var GoogleStrategy = require('../lib/strategy')
+  , fs = require('fs');
+
+
+describe('Strategy#userProfile', function() {
+  
+  describe('error caused by malformed response', function() {
+    var strategy = new GoogleStrategy({
+      consumerKey: 'www.example.com',
+      consumerSecret: 'secret'
+    }, function verify(){});
+    
+    strategy._oauth.get = function(url, token, tokenSecret, callback) {
+      var body = 'Hello, world.';
+      callback(null, body, undefined);
+    }
+    
+    
+    var err, profile;
+    
+    before(function(done) {
+      strategy.userProfile('token', 'token-secret', { user_id: '123' }, function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('Failed to parse user profile');
+    });
+    
+    it('should not supply profile', function() {
+      expect(profile).to.be.undefined;
+    });
+  }); // error caused by malformed response
+  
+  describe('internal error', function() {
+    var strategy = new GoogleStrategy({
+      consumerKey: 'www.example.com',
+      consumerSecret: 'secret'
+    }, function verify(){});
+    
+    strategy._oauth.get = function(url, token, tokenSecret, callback) {
+      return callback(new Error('something went wrong'));
+    }
+    
+    
+    var err, profile;
+    before(function(done) {
+      strategy.userProfile('token', 'token-secret', { user_id: '123' }, function(e, p) {
+        err = e;
+        profile = p;
+        done();
+      });
+    });
+  
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.constructor.name).to.equal('InternalOAuthError');
+      expect(err.message).to.equal('Failed to fetch user profile');
+      expect(err.oauthError).to.be.an.instanceOf(Error);
+      expect(err.oauthError.message).to.equal('something went wrong');
+    });
+  
+    it('should not supply profile', function() {
+      expect(profile).to.be.undefined;
+    });
+  }); // internal error
+  
+});
